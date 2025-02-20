a5a964c1ac2d355787b9d3d3c9d56385961f6d0d
HarshithaS1998 <121337849+HarshithaS1998@users.noreply.github.com>
2025-02-20 16:00:57 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,1 +1,11 @@
-
+aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.68.0"
+    }
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
